f927af6b983abc8642f8ad07edc919221178691c
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-01 23:29:42 +0300

    Add various imports form app folder, flask_script and flask migrate.

---
+++ app.py
@@ -0,0 +1,13 @@
+from app import create_app, db
+from flask_script import Manager, Shell, Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('serve', Server)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
