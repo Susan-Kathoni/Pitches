91ba56c23d69b9be85fe18fbdeaf43ee6f371758
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-11-02 23:44:29 +0300

    Add email configurations.

--- config.py
+++ config.py
@@ -6,8 +6,15 @@ class Config:
     General configuration parent class
     """
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
-    SECRET_KEY = 'susan123?'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
